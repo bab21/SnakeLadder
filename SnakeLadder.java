0764e8d695324f2ee8a0981480760b4559bceb40
bab21 <2016061@iiitdmj.ac.in>
2020-09-22 21:58:05 +0530

    ensuring player gets to winning and exact position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -12,11 +12,19 @@ public class SnakeLadder{
 		int max=6;
 		
 		
+		while(pos_player1!=100){
+			
 		// first_player_value is dice played by fisrt player
 		int first_player_value=(int)(Math.random()*(max-min+1)+min);
 		// choice1  is player 1 for any number between 0-2 
 		int choice1=(int)(Math.random()*(3)+0);
-		
+		if(choice1==LADDER)
+			pos_player1=(pos_player1+first_player_value>100)?pos_player1:pos_player1+first_player_value;
+		if(choice1==SNAKE)
+			pos_player1=(pos_player1-first_player_value<0)?0:pos_player1-first_player_value;
+		else if(choice1==NO_PLAY)
+			pos_player1=pos_player1+0;
+		}
 		
 	}
 }
