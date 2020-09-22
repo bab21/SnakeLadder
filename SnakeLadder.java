a3795077f072011ff2e17a669bb293799a00eb1d
bab21 <2016061@iiitdmj.ac.in>
2020-09-22 21:40:01 +0530

    Random number generation

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,9 +1,19 @@
 public class SnakeLadder{
+	public static final int NO_PLAY=0;
+	public static final int LADDER=1;
+	public static final int SNAKE=2;
+	
 	public static void main(String[] args){
 		
 		System.out.println("SnakeLadder game");
 		int pos_player1=0;
 		
+		int min=1;
+		int max=6;
+		
+		
+		// first_player_value is dice played by fisrt player
+		int first_player_value=(int)(Math.random()*(max-min+1)+min);
 		
 		
 	}
