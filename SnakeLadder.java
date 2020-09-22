df23f0433bdeabe875871fa585ca99ad52d654aa
bab21 <2016061@iiitdmj.ac.in>
2020-09-23 00:47:48 +0530

    Two Player Game

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,37 +3,84 @@ public class SnakeLadder{
 	public static final int LADDER=1;
 	public static final int SNAKE=2;
 	
+	
 	public static void main(String[] args){
-		
-		System.out.println("SnakeLadder game");
 		int pos_player1=0;
+		int pos_player2=0;
 		
 		int min=1;
 		int max=6;
-		// number of times dice was rolled...
-		int num_times=0;
 		
 		
-		
-		while(pos_player1!=100){
-		
-		System.out.println("Position after current update-- "+pos_player1);
-		
-		// first_player_value is dice played by fisrt player
-		int first_player_value=(int)(Math.random()*(max-min+1)+min);
-		num_times++;
-		// choice1  is player 1 for any number between 0-2 
-		int choice1=(int)(Math.random()*(3)+0);
-		if(choice1==LADDER)
-			pos_player1=(pos_player1+first_player_value>100)?pos_player1:pos_player1+first_player_value;
-		if(choice1==SNAKE)
-			pos_player1=(pos_player1-first_player_value<0)?0:pos_player1-first_player_value;
-		else if(choice1==NO_PLAY)
-			pos_player1=pos_player1+0;
+		while(true){
+			
+			int first_player_value=(int)(Math.random()*(max-min+1)+min);
+			//System.out.println("Dice value for player1:"+first_player_value);
+			int choice1=(int)(Math.random()*(3)+0);
+			//System.out.println("Choice for player 1:"+choice1);
+			
+			// if choice is ladder then player again rolls the dice ..
+			while(choice1==LADDER)
+				{    
+				pos_player1=(pos_player1+first_player_value>100)?pos_player1:pos_player1+first_player_value;
+					 if(pos_player1==100)
+					 {
+						 System.out.println("Winner is Player 1");
+						 return;
+					 }
+					 
+					 first_player_value=(int)(Math.random()*(max-min+1)+min);
+					 choice1=(int)(Math.random()*(3)+0);
+					 //System.out.println("Dice value for player 1:"+first_player_value);
+					 
+					 //System.out.println("Choice for player 1:"+choice1);
+					 //System.out.println("Position of player 1:"+pos_player1);
+				 
+				}
+			
+				if(choice1==SNAKE)
+					pos_player1=(pos_player1-first_player_value<0)?0:pos_player1-first_player_value;
+				if(choice1==NO_PLAY)
+					pos_player1=pos_player1+0;
+				
+				System.out.println("Position of player 1:"+pos_player1);
+				
+				
+			
+			if(pos_player1==100)
+			{System.out.println("Winner is Player 1");
+				return;
+			}
+			
+			int second_player_value=(int)(Math.random()*(max-min+1)+min);
+			int choice2=(int)(Math.random()*(3)+0);
+			
+			while(choice2==LADDER)
+				{ pos_player2=(pos_player2+second_player_value>100)?pos_player2:pos_player2+second_player_value;
+					 if(pos_player2==100)
+					 {
+						 System.out.println("Winner is Player 2");
+						 return;
+					 }
+					 second_player_value=(int)(Math.random()*(max-min+1)+min);
+					 choice2=(int)(Math.random()*(3)+0);
+				 
+				}
+			
+				if(choice2==SNAKE)
+					pos_player2=(pos_player2-second_player_value<0)?0:pos_player2-second_player_value;
+				if(choice2==NO_PLAY)
+					pos_player2=pos_player2+0;
+				
+			
+			if(pos_player2==100)
+			{System.out.println("Winner is Player 2");
+				return;
+			}
+			
+			
+			
 		}
-		
-		System.out.println("Number of times dice played  "+num_times);
-		
 		
 	}
 }
